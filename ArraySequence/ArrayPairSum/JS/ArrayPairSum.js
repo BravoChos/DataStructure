fb4e4fc469cbcfe7fa8aab3c8427c768fd8c827b
Song Cho <deeplearner507@gmail.com>
2018-12-09 16:27:32 +0900

    question

---
+++ ArraySequence/ArrayPairSum/JS/ArrayPairSum.js
@@ -0,0 +1,54 @@
+/*
+ArrayPairSum
+
+Given an array of numbers sorted in ascending order (least to greatest), 
+and a separate number (a "sum"), determine if any 2 numbers in the array add up to the sum.
+Return true if any 2 different numbers within the array add up to sum. 
+Return false if no 2 numbers in the array add up to sum. 
+*/
+
+//you solution starts here;
+
+
+
+
+
+
+
+
+
+
+
+// // naive version
+// function pairSum(arr, target) {
+// 	for (var i = 0; i < arr.length; i++) {
+// 			for (var j = i + 1; j < arr.length; j++) {
+// 					if (arr[i] + arr[j] === target) return true;    
+// 			}
+// 	}
+// 	return false;
+// }
+
+// //question: if we had a pointer on each end and the sum of the 2 numbers was less than the target 
+// //what adjustment would we want to make? answer: move left pointer up one 
+// // optimized version
+// function pairSum2(arr, target) {
+// 	var leftIdx = 0, rightIdx = arr.length - 1;
+	
+// 	if (arr.length < 2) return false; // edge case
+	
+// 	while (leftIdx !== rightIdx) {
+// 			var currSum = arr[leftIdx] + arr[rightIdx];
+			
+// 			if (currSum === target) return true;
+// 			else if (currSum < target) leftIdx++;
+// 			else rightIdx--;
+// 	}
+	
+// 	return false
+// }
+
+// console.log(pairSum([1, 1, 2, 3, 4, 5], 7) ) // Should return true
+// console.log(pairSum([1, 2, 3, 4, 5], 9) ) // Should return true
+// console.log(pairSum([1, 2, 3, 7, 8], 7) ) // Should return false
+// console.log(pairSum([1, 2, 3, 4, 5], 2) ) // Should return false
---
+++ ArraySequence/ArrayPairSum/python/ArrayPairSum.py
@@ -0,0 +1,22 @@
+# ArrayPairSum Check
+# Problem
+# Given an integer array, output all the unique pairs that sum up to a specific value k.
+
+# So the input:
+
+# pair_sum([1,3,2,2],4)
+
+# would return 2 pairs:
+
+#  (1,3)
+#  (2,2)
+
+# note: FOR TESTING PURPOSES CHANGE YOUR FUNCTION SO IT OUTPUTS THE NUMBER OF PAIRS
+
+# Solution
+# Fill out your solution below:
+
+def pair_sum(arr,k):
+    
+    pass
+print( pair_sum([1,3,2,2],4) ) #2
