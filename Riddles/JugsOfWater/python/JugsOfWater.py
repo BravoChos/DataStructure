1cb7339348c706e559c0705b2eaa8dac3516550c
Song Cho <deeplearner507@gmail.com>
2019-03-22 23:54:05 +0900

    Jugs of Water

---
+++ Riddles/JugsOfWater/python/JugsOfWater.py
@@ -0,0 +1,22 @@
+'''
+Jugs of Water 
+Problem Statement
+You have a five gallons jug and a three gallons jug, and an unlimited supply of water 
+(but no measuring cups) How would you come up with exactly four gallons of water?
+
+solution                                            amount after action               
+                                                 3-gal jug     5-gal jug             
+Fill 5-gallan jug                                      -            5
+
+Fill up 3-gallon jug with water from 5-gallon jug      3            2  
+
+Pour out 3-gallon jug                                  0            2                       
+
+Transfer water from 5-gallon jug to 3-gallon jug       2            0                  
+
+Fill up 5-gallon jug                                   2            5              
+
+Transfer water from 5-gallon jug to 3-gallon jug       3            4 
+
+
+'''
